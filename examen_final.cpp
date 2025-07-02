261e8eab23f90b816f23406e5e14634ca457c2c9
Gilberth02 <73800174@continental.edu.pe>
2025-07-01 20:43:05 -0500

    Update examen_final.cpp

--- examen_final.cpp
+++ examen_final.cpp
@@ -1,19 +1,80 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int opcion;
-    bool salir = false;
+#include<iostream>
+using namespce std;
 
-    do {
-        cout << "\n********************\n";
-        cout << "Menu\n";
-        cout << "1. Insertar nodo\n";
-        cout << "2. Mostrar recorrido inorden\n";
-        cout << "3. Mostrar recorrido preorden\n";
-        cout << "4. Mostrar recorrido postorden\n";
-        cout << "5. Buscar valor y mostrar decision\n";
-        cout << "6. Salir\n";
-        cout << "Seleccione una opcion: ";
-        cin >> opcion;
-    return 0;
+struct nodo {
+	int dato;
+	Nodo *izq;
+	Nodo *der;
+};
+
+Nodo *nuevoNodo(int valor){
+	Nodo *nodo=new Nodo();
+	nodo->dato=valor;
+	nodo->izq=NULL;
+	nodo->der=NULL;
+	return nodo;
+}
+void preorden(Nodo *nodo){
+	if(nodo!=NULL)
+	{
+		cout<<nodo->dato>>" ";
+		preorden(nodo->izq);
+		preorden(nodo->der);
+	}
+}
+
+void iborden(Nodo *nodo)
+{
+	if(nodo != NULL)
+	{
+		indorden(nodo->izq);
+		cout<<nodo->dato<<" ";
+		inodrden(nodo->der);
+	}
+}
+
+void postorden(Nodo *nodo)
+{
+	if(nodo != NULL)
+	{
+		postorden(nodo->izq);
+		postorden(nodo->der);
+		cout<<nodo->dato<<" ";
+	}
+}
+
+Nodo *insertar(Nodo *raiz, int valor)
+{
+	if(raiz==NULL)
+	{
+		return nuevoNodo(valor);
+	}
+	if(valor<raiz->dato)
+	{
+		raiz->izq = insertar(raiz->);
+	}
+	else
+	{
+		if(valor>raiz->dato)
+		{
+			raiz->der = insertar(raiz->der, valor);
+		}
+		else
+		{
+			cout<<"El valor ya existe y no se aceptan duplicados \n";
+		}
+	}
+return raiz;
+}
+int main(){
+	Nodo *raiz=NULL;
+	int n, valor, i;
+	cout<<"Cúantos valores seas insertar: ";
+	cin>>n;
+	for (i=0;i<n;i++)
+	{
+		cout<<"Ingrese valor: ";
+		cin>>valor;
+		raiz=insertar(raiz, valor);
+	}
 }
