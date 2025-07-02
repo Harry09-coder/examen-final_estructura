3e1ca1133a426da66f2e6685030720cfaf2dbfd6
61239779 <61239779@continental.edu.pe>
2025-07-01 20:41:43 -0500

    Update examen_final.cpp
    menu

---
+++ examen_final.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int opcion;
+    bool salir = false;
+
+    do {
+        cout << "\n********************\n";
+        cout << "Menu\n";
+        cout << "1. Insertar nodo\n";
+        cout << "2. Mostrar recorrido inorden\n";
+        cout << "3. Mostrar recorrido preorden\n";
+        cout << "4. Mostrar recorrido postorden\n";
+        cout << "5. Buscar valor y mostrar decision\n";
+        cout << "6. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+    return 0;
+}
